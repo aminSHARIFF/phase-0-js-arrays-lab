bd2ecf81aa8da9d6264e4da18386f23df4b91f87
aminSHARIFF <amin29536@gmail.com>
2025-11-20 23:12:01 +0300

    arrays lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,26 @@
-// Write your code here
+/* 1. Create the products array */
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+/* 2. Function to log the first product */
+function logFirstProduct() {
+console.log(products[0]);
+}
+
+/* 3. Function to add a new product */
+function addProduct(productName) {
+products.push(productName);
+}
+
+/* 4. Function to update a product name */
+function updateProductName(position, newName) {
+products[position] = newName;
+}
+
+/* 5. Function to remove the last product */
+function removeLastProduct() {
+products.pop();
+}
+
 
 
 
